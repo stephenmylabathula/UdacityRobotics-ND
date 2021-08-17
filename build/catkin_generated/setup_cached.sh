f9ea6eb79394343490f933701862ab44f95cc0b1
Stephen M <mylo777@icloud.com>
2021-08-16 21:31:00 -0700

    full ekf

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/robond/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/robond/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/robond/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/robond/catkin_ws/src:$ROS_PACKAGE_PATH"
